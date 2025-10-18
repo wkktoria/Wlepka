d302d7bbb7b18841821dc0986f70d018f52575a0
Wiktoria Peretiatkowicz <127852624+wkktoria@users.noreply.github.com>
2025-10-18 21:08:12 +0200

    Create header component

---
+++ wlepka-ui/src/components/Header.jsx
@@ -0,0 +1,41 @@
+import { faShoppingBasket, faTags } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+const Header = () => {
+  return (
+    <header className="header">
+      <div className="container">
+        <a href="/" className="link">
+          <FontAwesomeIcon icon={faTags} className="fa-icon" />
+          <span className="brand-title">Wlepka</span>
+        </a>
+        <nav className="nav">
+          <ul>
+            <li>
+              <a href="/about" className="nav-link">
+                Home
+              </a>
+            </li>
+            <li>
+              <a href="/contact" className="nav-link">
+                Contact
+              </a>
+            </li>
+            <li>
+              <a href="/login" className="nav-link">
+                Login
+              </a>
+            </li>
+            <li>
+              <a href="/cart" className="nav-link">
+                <FontAwesomeIcon icon={faShoppingBasket} />
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
