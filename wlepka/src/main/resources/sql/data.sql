6daaf54f7e81a85dd1ed4da7805bb31dc4df2c36
Wiktoria Peretiatkowicz <127852624+wkktoria@users.noreply.github.com>
2025-10-19 21:13:30 +0200

    Set up H2 DB

---
+++ wlepka/src/main/resources/sql/data.sql
@@ -0,0 +1,8 @@
+INSERT INTO products(name, description, price, popularity, image_url, created_at, created_by, updated_at, updated_by)
+VALUES ('Toxic', 'Toksyczny gołąb.', 10, 85, '/stickers/toxic.jpg', CURRENT_TIMESTAMP, 'admin', NULL, NULL);
+
+INSERT INTO products(name, description, price, popularity, image_url, created_at, created_by, updated_at, updated_by)
+VALUES ('Angels', 'Szalone aniołki.', 8, 70, '/stickers/angels.jpg', CURRENT_TIMESTAMP, 'admin', NULL, NULL);
+
+INSERT INTO products(name, description, price, popularity, image_url, created_at, created_by, updated_at, updated_by)
+VALUES ('Jeep', 'Piesek jadący Jeepem.', 5, 60, '/stickers/jeep.jpg', CURRENT_TIMESTAMP, 'admin', NULL, NULL);
