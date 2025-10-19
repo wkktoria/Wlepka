0bd2e4b26576bb50dc9088cf5c9d5aefd0718373
Wiktoria Peretiatkowicz <127852624+wkktoria@users.noreply.github.com>
2025-10-19 20:35:56 +0200

    Init backend project

---
+++ wlepka/src/main/java/io/github/wkktoria/wlepka/WlepkaApplication.java
@@ -0,0 +1,13 @@
+package io.github.wkktoria.wlepka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WlepkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WlepkaApplication.class, args);
+	}
+
+}
---
+++ wlepka/src/test/java/io/github/wkktoria/wlepka/WlepkaApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.wkktoria.wlepka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WlepkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
