9b6ed47e259926ddbbd733205743bafaa5d5f1f5
Wiktoria Peretiatkowicz <127852624+wkktoria@users.noreply.github.com>
2025-10-19 22:20:03 +0200

    Connect frontend with backend

--- wlepka-ui/src/components/Home.jsx
+++ wlepka-ui/src/components/Home.jsx
@@ -1,8 +1,20 @@
-import products from "../data/products";
 import PageHeading from "./PageHeading";
 import ProductListings from "./ProductListings";
+import apiClient from "../api/apiClient";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+
+  const fetchProducts = async () => {
+    const response = await apiClient.get("/products");
+    setProducts(response.data);
+  };
+
   return (
     <div className="max-w-[1152px] mx-auto px-6 py-8">
       <PageHeading title="Poznaj nasze wlepki">
