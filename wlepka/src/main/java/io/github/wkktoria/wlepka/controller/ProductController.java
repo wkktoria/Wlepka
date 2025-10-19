ae5372f359c33ee6a72494afb045e5e7bbc6b3e5
Wiktoria Peretiatkowicz <127852624+wkktoria@users.noreply.github.com>
2025-10-19 20:41:25 +0200

    Create products controller

---
+++ wlepka/src/main/java/io/github/wkktoria/wlepka/controller/ProductController.java
@@ -0,0 +1,16 @@
+package io.github.wkktoria.wlepka.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1/products")
+public class ProductController {
+
+    @GetMapping
+    public String getProducts() {
+        return "Produkciki";
+    }
+
+}
