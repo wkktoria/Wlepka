47f02a3cf515694fab33bf7c941f3c6dbc635e5c
Wiktoria Peretiatkowicz <127852624+wkktoria@users.noreply.github.com>
2025-10-20 23:01:32 +0200

    Create search box component

---
+++ wlepka-ui/src/components/SearchBox.jsx
@@ -0,0 +1,13 @@
+export default function SearchBox({ label, placeholder, value }) {
+  return (
+    <div className="flex items-center gap-3 pl-4 flex-1 font-primary">
+      <label className="text-lg font-semibold text-primary">{label}</label>
+      <input
+        type="text"
+        className="px-4 py-2 text-base border rounded-md transition border-primary focus:ring focus:ring-dark focus:outline-none text-gray-800"
+        placeholder={placeholder}
+        value={value}
+      />
+    </div>
+  );
+}
