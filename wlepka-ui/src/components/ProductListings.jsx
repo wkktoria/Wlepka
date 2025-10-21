3ffd45679113dc3515c675adb475a7b245edb022
Wiktoria Peretiatkowicz <127852624+wkktoria@users.noreply.github.com>
2025-10-21 15:01:35 +0200

    Add search box and dropdown to homepage

--- wlepka-ui/src/components/ProductListings.jsx
+++ wlepka-ui/src/components/ProductListings.jsx
@@ -1,11 +1,68 @@
+import { useState } from "react";
+import Dropdown from "./Dropdown";
 import ProductCard from "./ProductCard";
+import SearchBox from "./SearchBox";
+
+const sortList = ["Popularność", "Cena od najniższej", "Cena od najwyższej"];
 
 export default function ProductListings({ products }) {
+  const [searchText, setSearchText] = useState("");
+  const [selectedSort, setSelectedSort] = useState(sortList[0]);
+
+  let filteredAndSortedProducts = Array.isArray(products)
+    ? products.filter(
+        (product) =>
+          product.name.toLowerCase().includes(searchText.toLowerCase()) ||
+          product.description.toLowerCase().includes(searchText.toLowerCase())
+      )
+    : [];
+
+  switch (selectedSort) {
+    case sortList[2]:
+      filteredAndSortedProducts = filteredAndSortedProducts.sort(
+        (a, b) => parseFloat(b.price) - parseFloat(a.price)
+      );
+      break;
+    case sortList[1]:
+      filteredAndSortedProducts = filteredAndSortedProducts.sort(
+        (a, b) => parseFloat(a.price) - parseFloat(b.price)
+      );
+      break;
+    case sortList[0]:
+    default:
+      filteredAndSortedProducts = filteredAndSortedProducts.sort(
+        (a, b) => parseInt(b.popularity) - parseInt(a.popularity)
+      );
+      break;
+  }
+
+  const handleSearchChange = (inputSearch) => {
+    setSearchText(inputSearch);
+  };
+
+  const handleSortChange = (sortType) => {
+    setSelectedSort(sortType);
+  };
+
   return (
     <div className="max-w-[1152px] mx-auto">
+      <div className="flex flex-col sm:flex-row justify-between items-center gap-4 pt-12">
+        <SearchBox
+          label="Szukaj"
+          placeholder="Szukaj naklejek..."
+          value={searchText}
+          handleSearch={(value) => handleSearchChange(value)}
+        />
+        <Dropdown
+          label="Sortuj"
+          options={sortList}
+          value={sortList[0]}
+          handleSort={(value) => handleSortChange(value)}
+        />
+      </div>
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gapy-8 gap-x-6 py-12">
-        {products.length > 0 ? (
-          products.map((product) => (
+        {filteredAndSortedProducts.length > 0 ? (
+          filteredAndSortedProducts.map((product) => (
             <ProductCard key={product.productId} product={product} />
           ))
         ) : (
--- wlepka-ui/src/components/SearchBox.jsx
+++ wlepka-ui/src/components/SearchBox.jsx
@@ -1,4 +1,4 @@
-export default function SearchBox({ label, placeholder, value }) {
+export default function SearchBox({ label, placeholder, value, handleSearch }) {
   return (
     <div className="flex items-center gap-3 pl-4 flex-1 font-primary">
       <label className="text-lg font-semibold text-primary">{label}</label>
@@ -7,6 +7,7 @@ export default function SearchBox({ label, placeholder, value }) {
         className="px-4 py-2 text-base border rounded-md transition border-primary focus:ring focus:ring-dark focus:outline-none text-gray-800"
         placeholder={placeholder}
         value={value}
+        onChange={(event) => handleSearch(event.target.value)}
       />
     </div>
   );
