61a66709877942b3e178c80b9e6985941b68add0
Wiktoria Peretiatkowicz <127852624+wkktoria@users.noreply.github.com>
2025-10-23 15:11:15 +0200

    Implement cart component

--- wlepka-ui/src/components/Cart.jsx
+++ wlepka-ui/src/components/Cart.jsx
@@ -1,3 +1,29 @@
+import { Link } from "react-router-dom";
+import PageTitle from "./PageTitle";
+import emptyCartImage from "../assets/util/empty-cart.png";
+
 export default function Cart() {
-  return <div>Cart</div>;
+  return (
+    <div className="min-h-[852px] py-12 bg-normalbg dark:bg-darkbg font-primary">
+      <div className="max-w-4xl mx-auto px-4">
+        <PageTitle title="Koszyk" />
+        <div className="text-center text-gray-600 dark:text-lighter flex flex-col items-center">
+          <p className="max-w-[576px] px-2 mx-auto text-base mb-4">
+            Wygląda na to, że Twój koszyk jest pusty.
+          </p>
+          <img
+            src={emptyCartImage}
+            alt="Empty cart"
+            className="max-w-[300px] mx-auto mb-6 dark:bg-light dark:rounded-md"
+          />
+          <Link
+            to="/"
+            className="py-2 px-4 bg-primary dark:bg-light text-white dark:text-black text-xl font-semibold rounded-sm flex justify-center items-center hover:bg-dark dark:hover:bg-lighter transition"
+          >
+            Powrót do strony głównej
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 }
