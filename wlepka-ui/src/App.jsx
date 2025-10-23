9c7c443554a9c68e582195f4f8b36b044c45c02b
Wiktoria Peretiatkowicz <127852624+wkktoria@users.noreply.github.com>
2025-10-23 21:11:58 +0200

    Improve navigation

--- wlepka-ui/src/App.jsx
+++ wlepka-ui/src/App.jsx
@@ -1,13 +1,23 @@
 import { Outlet } from "react-router-dom";
 import Footer from "./components/footer/Footer";
 import Header from "./components/header/Header";
-import Home from "./components/Home";
+import { useNavigation } from "react-router-dom";
 
 function App() {
+  const navigation = useNavigation();
+
   return (
     <>
       <Header />
-      <Outlet />
+      {navigation.state === "loading" ? (
+        <div className="flex items-center justify-center min-h-[852px]">
+          <span className="text-2xl font-semibold text-primary dark:text-light">
+            Ładowanie...
+          </span>
+        </div>
+      ) : (
+        <Outlet />
+      )}
       <Footer />
     </>
   );
--- wlepka-ui/src/components/Cart.jsx
+++ wlepka-ui/src/components/Cart.jsx
@@ -1,8 +1,14 @@
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import PageTitle from "./PageTitle";
 import emptyCartImage from "../assets/util/empty-cart.png";
 
 export default function Cart() {
+  const navigation = useNavigate();
+
+  const handleClick = () => {
+    navigation("/home");
+  };
+
   return (
     <div className="min-h-[852px] py-12 bg-normalbg dark:bg-darkbg font-primary">
       <div className="max-w-4xl mx-auto px-4">
@@ -16,12 +22,12 @@ export default function Cart() {
             alt="Empty cart"
             className="max-w-[300px] mx-auto mb-6 dark:bg-light dark:rounded-md"
           />
-          <Link
-            to="/"
+          <button
+            onClick={handleClick}
             className="py-2 px-4 bg-primary dark:bg-light text-white dark:text-black text-xl font-semibold rounded-sm flex justify-center items-center hover:bg-dark dark:hover:bg-lighter transition"
           >
             Powrót do strony głównej
-          </Link>
+          </button>
         </div>
       </div>
     </div>
