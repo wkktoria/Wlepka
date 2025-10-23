045ff8e81dd1b45b7008a0b9daee27d18da8d7e3
Wiktoria Peretiatkowicz <127852624+wkktoria@users.noreply.github.com>
2025-10-23 21:16:16 +0200

    Update database schema

--- wlepka/src/main/resources/sql/schema.sql
+++ wlepka/src/main/resources/sql/schema.sql
@@ -11,3 +11,16 @@ CREATE TABLE IF NOT EXISTS products
     updated_at TIMESTAMP DEFAULT NULL,
     updated_by VARCHAR(20) DEFAULT NULL
 );
+
+CREATE TABLE IF NOT EXISTS contacts
+(
+    contact_id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    mobile_number VARCHAR(9) NOT NULL,
+    message VARCHAR(500),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    created_by VARCHAR(20) NOT NULL,
+    updated_at TIMESTAMP DEFAULT NULL,
+    updated_by VARCHAR(20) DEFAULT NULL
+);
