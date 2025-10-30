2f862285b7e79f364f05593c72d46aabe2ce94fc
Wiktoria Peretiatkowicz <127852624+wkktoria@users.noreply.github.com>
2025-10-30 21:39:11 +0100

    Update contact entity

--- wlepka/src/main/java/io/github/wkktoria/wlepka/entity/Contact.java
+++ wlepka/src/main/java/io/github/wkktoria/wlepka/entity/Contact.java
@@ -1,9 +1,6 @@
 package io.github.wkktoria.wlepka.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -16,6 +13,7 @@ import java.time.Instant;
 public class Contact {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "contact_id", nullable = false)
     private Long id;
 
