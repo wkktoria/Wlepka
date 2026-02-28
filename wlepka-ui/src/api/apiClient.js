61ca336176298befd8c9baca35ff9a117bc625e4
Wiktoria Peretiatkowicz <127852624+wkktoria@users.noreply.github.com>
2026-02-28 21:05:17 +0100

    Add dependencies and initial config for security

--- wlepka-ui/src/api/apiClient.js
+++ wlepka-ui/src/api/apiClient.js
@@ -2,6 +2,9 @@ import axios from "axios";
 
 const apiClient = axios.create({
   baseURL: import.meta.env.VITE_API_BASE_URL,
+  headers: {
+    Accept: "application/json"
+  },
   timeout: 10000,
 });
 
