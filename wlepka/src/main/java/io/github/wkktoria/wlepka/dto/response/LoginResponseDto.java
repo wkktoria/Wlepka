c28a168a0c9e7692023dfcbe88a9ffd4888543ac
Wiktoria Peretiatkowicz <127852624+wkktoria@users.noreply.github.com>
2026-05-30 21:14:06 +0200

    Rename field in login response

--- wlepka/src/main/java/io/github/wkktoria/wlepka/dto/response/LoginResponseDto.java
+++ wlepka/src/main/java/io/github/wkktoria/wlepka/dto/response/LoginResponseDto.java
@@ -2,5 +2,5 @@ package io.github.wkktoria.wlepka.dto.response;
 
 import io.github.wkktoria.wlepka.dto.UserDto;
 
-public record LoginResponseDto(String message, UserDto userDto, String token) {
+public record LoginResponseDto(String message, UserDto user, String token) {
 }
