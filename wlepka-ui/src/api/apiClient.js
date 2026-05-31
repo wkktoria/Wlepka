8e55dfa185e6da0d6ff5c4d2b34a7ae48e38e79e
Wiktoria Peretiatkowicz <127852624+wkktoria@users.noreply.github.com>
2026-05-31 15:52:10 +0200

    Use token

--- wlepka-ui/src/api/apiClient.js
+++ wlepka-ui/src/api/apiClient.js
@@ -3,9 +3,23 @@ import axios from "axios";
 const apiClient = axios.create({
   baseURL: import.meta.env.VITE_API_BASE_URL,
   headers: {
-    Accept: "application/json"
+    Accept: "application/json",
+    "Content-Type": "application/json",
   },
   timeout: 10000,
 });
+
+apiClient.interceptors.request.use(
+  async (config) => {
+    const token = localStorage.getItem("token");
+
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+
+    return config;
+  },
+  (error) => Promise.reject(error),
+);
 
 export default apiClient;
