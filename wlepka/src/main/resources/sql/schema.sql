b0a8e39569ab84c8a0d6b8fb100eaaea6c260015
Wiktoria Peretiatkowicz <127852624+wkktoria@users.noreply.github.com>
2026-05-31 11:06:54 +0200

    Create customers table

--- wlepka/src/main/resources/sql/schema.sql
+++ wlepka/src/main/resources/sql/schema.sql
@@ -1,26 +1,41 @@
 CREATE TABLE IF NOT EXISTS products
 (
-    product_id BIGINT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(250) NOT NULL,
-    description VARCHAR(500) NOT NULL,
-    price DECIMAL(10, 2) NOT NULL,
-    popularity INT NOT NULL,
-    image_url VARCHAR(500),
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
-    created_by VARCHAR(20) NOT NULL,
-    updated_at TIMESTAMP DEFAULT NULL,
-    updated_by VARCHAR(20) DEFAULT NULL
+    product_id  BIGINT AUTO_INCREMENT PRIMARY KEY,
+    name        VARCHAR(250)                          NOT NULL,
+    description VARCHAR(500)                          NOT NULL,
+    price       DECIMAL(10, 2)                        NOT NULL,
+    popularity  INT                                   NOT NULL,
+    image_url   VARCHAR(500),
+    created_at  TIMESTAMP   DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    created_by  VARCHAR(20)                           NOT NULL,
+    updated_at  TIMESTAMP   DEFAULT NULL,
+    updated_by  VARCHAR(20) DEFAULT NULL
 );
 
 CREATE TABLE IF NOT EXISTS contacts
 (
-    contact_id BIGINT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(30) NOT NULL,
-    email VARCHAR(100) NOT NULL,
-    mobile_number VARCHAR(9) NOT NULL,
-    message VARCHAR(500) NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
-    created_by VARCHAR(20) NOT NULL,
-    updated_at TIMESTAMP DEFAULT NULL,
-    updated_by VARCHAR(20) DEFAULT NULL
+    contact_id    BIGINT AUTO_INCREMENT PRIMARY KEY,
+    name          VARCHAR(30)                           NOT NULL,
+    email         VARCHAR(100)                          NOT NULL,
+    mobile_number VARCHAR(9)                            NOT NULL,
+    message       VARCHAR(500)                          NOT NULL,
+    created_at    TIMESTAMP   DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    created_by    VARCHAR(20)                           NOT NULL,
+    updated_at    TIMESTAMP   DEFAULT NULL,
+    updated_by    VARCHAR(20) DEFAULT NULL
+);
+
+CREATE TABLE IF NOT EXISTS customers
+(
+    customer_id   BIGINT AUTO_INCREMENT PRIMARY KEY,
+    name          VARCHAR(30)                           NOT NULL,
+    email         VARCHAR(100)                          NOT NULL UNIQUE,
+    mobile_number VARCHAR(9)                            NOT NULL UNIQUE,
+    password_hash VARCHAR(500)                          NOT NULL,
+    created_at    TIMESTAMP   DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    created_by    VARCHAR(20)                           NOT NULL,
+    updated_at    TIMESTAMP   DEFAULT NULL,
+    updated_by    VARCHAR(20) DEFAULT NULL,
+    UNIQUE KEY unique_email (email),
+    UNIQUE KEY unique_mobile_number (mobile_number)
 );
