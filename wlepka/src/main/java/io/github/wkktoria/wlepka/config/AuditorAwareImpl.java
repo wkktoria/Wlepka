4a2431948a5a3cc3446ce4015dd8988c82682607
Wiktoria Peretiatkowicz <127852624+wkktoria@users.noreply.github.com>
2026-06-21 15:53:30 +0200

    Improve auditing

--- wlepka/src/main/java/io/github/wkktoria/wlepka/config/AuditorAwareImpl.java
+++ wlepka/src/main/java/io/github/wkktoria/wlepka/config/AuditorAwareImpl.java
@@ -1,6 +1,9 @@
 package io.github.wkktoria.wlepka.config;
 
+import io.github.wkktoria.wlepka.entity.Customer;
 import org.springframework.data.domain.AuditorAware;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 
 import java.util.Optional;
@@ -10,7 +13,23 @@ public class AuditorAwareImpl implements AuditorAware<String> {
 
     @Override
     public Optional<String> getCurrentAuditor() {
-        return Optional.of("Anonymous");
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        if (authentication == null || !authentication.isAuthenticated() ||
+                authentication.getPrincipal().equals("anonymousUser")) {
+            return Optional.of("Anonymous user");
+        }
+
+        Object principal = authentication.getPrincipal();
+        String username;
+
+        if (principal instanceof Customer customer) {
+            username = customer.getEmail();
+        } else {
+            username = principal.toString();
+        }
+
+        return Optional.of(username);
     }
 
 }
